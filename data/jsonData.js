6977654022d736d59a51753a35b31297fb97bd07
Lukas Simianer <lsimianer@gmail.com>
2019-12-07 11:49:37 -0600

    single quotes changed to double quotes per json conventions

--- data/jsonData.js
+++ data/jsonData.js
@@ -1,43 +1,43 @@
 [
     {
-        'developer': 'javascript',
-        'state': 'texas',
-        'age': '27'
+        "developer": "javascript",
+        "state": "texas",
+        "age": "27"
     },
     {
-        'developer': 'MERN',
-        'state': 'Alaska',
-        'age': '27'
+        "developer": "MERN",
+        "state": "Alaska",
+        "age": "27"
     },
     {
-        'developer': 'PHP',
-        'state': 'iowa',
-        'age': '45'
+        "developer": "PHP",
+        "state": "iowa",
+        "age": "45"
     },
     {
-        'developer': 'Front end',
-        'state': 'california',
-        'age': '25'
+        "developer": "Front end",
+        "state": "california",
+        "age": "25"
     },
     {
-        'developer': 'node',
-        'state': 'texas',
-        'age': '29'
+        "developer": "node",
+        "state": "texas",
+        "age": "29"
     },
     {
-        'developer': 'angular only',
-        'state': 'who cares',
-        'age': '50'
+        "developer": "angular only",
+        "state": "who cares",
+        "age": "50"
     },
     {
-        'developer': 'lazy',
-        'state': 'kansas',
-        'age': '37'
+        "developer": "lazy",
+        "state": "kansas",
+        "age": "37"
     },
     {
-        'developer': 'fullstack',
-        'state': 'virginia',
-        'age': '28'
+        "developer": "fullstack",
+        "state": "virginia",
+        "age": "28"
     }
 ]
 /Users/lukassimianer/Desktop/apiMaker/developer-tool-api/data/jsonData.js
